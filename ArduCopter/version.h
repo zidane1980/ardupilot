9066dedf60bc9e8a3e159632daa74b2abfab0011
bnsgeyer <bnsgeyer@aol.com>
2017-04-25 21:33:44 -0400

    Release of Copter-3.4.6 Geyer V1.1-rc2

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "APM:Copter V3.4.6 Geyer V1.1-rc1"
+#define THISFIRMWARE "APM:Copter V3.4.6 Geyer V1.1-rc2"
 #define FIRMWARE_VERSION 3,4,6,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #ifndef GIT_VERSION
