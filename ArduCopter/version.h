badbcf1a11b750f15cf53d08fee4f6a01e1cfe94
bnsgeyer <bnsgeyer@aol.com>
2018-01-15 13:40:15 -0500

    Copter: release of Copter-3.5.4 bnsgeyerv1.1

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "APM:Copter V3.5.4 bnsgeyer V1.0"
+#define THISFIRMWARE "APM:Copter V3.5.4 bnsgeyer V1.1"
 #define FIRMWARE_VERSION 3,5,4,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #ifndef GIT_VERSION
